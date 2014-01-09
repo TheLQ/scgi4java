5595a3aad1e5206381db9efb9cc66ce56d0adffd
Leon Blakey <lord.quackstar@gmail.com>
2014-01-09 12:29:42 -0500

    Moved constants to TestUtils

--- scgi/src/test/java/org/thelq/scgi4java/scgi/SCGIClientTest.java
+++ scgi/src/test/java/org/thelq/scgi4java/scgi/SCGIClientTest.java
@@ -3,12 +3,8 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-
 package org.thelq.scgi4java.scgi;
 
-import java.util.LinkedHashMap;
-import java.util.Map;
-import org.apache.commons.lang3.StringUtils;
 import org.testng.Assert;
 import org.testng.annotations.Test;
 import static org.thelq.scgi4java.scgi.SCGIClient.makeRequest;
@@ -20,63 +16,8 @@ import static org.thelq.scgi4java.scgi.SCGIClient.makeRequest;
 public class SCGIClientTest {
 	@Test
 	public void makeRequestTest() {
-		//Generated from nginx scgi_pass
-		String exampleEncoded = "730:CONTENT_LENGTH|81|"
-				+ "SCGI|1|"
-				+ "REQUEST_METHOD|POST|"
-				+ "REQUEST_URI|/SCGI|"
-				+ "QUERY_STRING||"
-				+ "CONTENT_TYPE|text/xml; charset=UTF-8|"
-				+ "DOCUMENT_URI|/SCGI|"
-				+ "DOCUMENT_ROOT|/usr/share/nginx/html|"
-				+ "SERVER_PROTOCOL|HTTP/1.1|"
-				+ "REMOTE_ADDR|55.555.5.55|"
-				+ "REMOTE_PORT|55555|"
-				+ "SERVER_PORT|80|"
-				+ "SERVER_NAME|localhost|"
-				+ "HTTP_HOST|55.555.555.55|"
-				+ "HTTP_USER_AGENT|Mozilla/5.0 (Windows NT 6.3; WOW64; rv:26.0) Gecko/20100101 Firefox/26.0|"
-				+ "HTTP_ACCEPT|application/xml, text/xml, */*; q=0.01|"
-				+ "HTTP_ACCEPT_LANGUAGE|en-US,en;q=0.5|"
-				+ "HTTP_ACCEPT_ENCODING|gzip, deflate|"
-				+ "HTTP_CONTENT_TYPE|text/xml; charset=UTF-8|"
-				+ "HTTP_X_REQUESTED_WITH|XMLHttpRequest|"
-				+ "HTTP_REFERER|http://55.555.555.55/test.html|"
-				+ "HTTP_CONTENT_LENGTH|81|"
-				+ "HTTP_CONNECTION|keep-alive|"
-				+ "HTTP_PRAGMA|no-cache|"
-				+ "HTTP_CACHE_CONTROL|no-cache|"
-				+ ",<methodCall><methodName>system.library_version</methodName><params/></methodCall>";
-		String exampleRaw = StringUtils.replace(exampleEncoded, "|", "\0");
-		
-		Map<String, String> requestMap = new LinkedHashMap<String, String>();
-		//requestMap.put("CONTENT_LENGTH", "81");
-		requestMap.put("REQUEST_METHOD", "POST");
-		requestMap.put("REQUEST_URI", "/SCGI");
-		requestMap.put("QUERY_STRING", "");
-		requestMap.put("CONTENT_TYPE", "text/xml; charset=UTF-8");
-		requestMap.put("DOCUMENT_URI", "/SCGI");
-		requestMap.put("DOCUMENT_ROOT", "/usr/share/nginx/html");
-		requestMap.put("SERVER_PROTOCOL", "HTTP/1.1");
-		requestMap.put("REMOTE_ADDR", "55.555.5.55");
-		requestMap.put("REMOTE_PORT", "55555");
-		requestMap.put("SERVER_PORT", "80");
-		requestMap.put("SERVER_NAME", "localhost");
-		requestMap.put("HTTP_HOST", "55.555.555.55");
-		requestMap.put("HTTP_USER_AGENT", "Mozilla/5.0 (Windows NT 6.3; WOW64; rv:26.0) Gecko/20100101 Firefox/26.0");
-		requestMap.put("HTTP_ACCEPT", "application/xml, text/xml, */*; q=0.01");
-		requestMap.put("HTTP_ACCEPT_LANGUAGE", "en-US,en;q=0.5");
-		requestMap.put("HTTP_ACCEPT_ENCODING", "gzip, deflate");
-		requestMap.put("HTTP_CONTENT_TYPE", "text/xml; charset=UTF-8");
-		requestMap.put("HTTP_X_REQUESTED_WITH", "XMLHttpRequest");
-		requestMap.put("HTTP_REFERER", "http://55.555.555.55/test.html");
-		requestMap.put("HTTP_CONTENT_LENGTH", "81");
-		requestMap.put("HTTP_CONNECTION", "keep-alive");
-		requestMap.put("HTTP_PRAGMA", "no-cache");
-		requestMap.put("HTTP_CACHE_CONTROL", "no-cache");
-		String requestBody = "<methodCall><methodName>system.library_version</methodName><params/></methodCall>";
-		String requestCreated = makeRequest(requestMap, requestBody);
-		
-		Assert.assertEquals(exampleRaw, requestCreated, "Generated request doesn't match given");
+		String requestCreated = makeRequest(TestUtils.REQUEST_HEADERS, TestUtils.REQUEST_BODY);
+
+		Assert.assertEquals(TestUtils.REQUEST_RAW, requestCreated, "Generated request doesn't match given");
 	}
 }
---
+++ scgi/src/test/java/org/thelq/scgi4java/scgi/TestUtils.java
@@ -0,0 +1,68 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package org.thelq.scgi4java.scgi;
+
+import com.google.common.collect.ImmutableMap;
+import org.apache.commons.lang3.StringUtils;
+
+/**
+ *
+ * @author Leon
+ */
+public class TestUtils {
+	public static final String REQUEST_RAW = StringUtils.replace("730:CONTENT_LENGTH|81|"
+			+ "SCGI|1|"
+			+ "REQUEST_METHOD|POST|"
+			+ "REQUEST_URI|/SCGI|"
+			+ "QUERY_STRING||"
+			+ "CONTENT_TYPE|text/xml; charset=UTF-8|"
+			+ "DOCUMENT_URI|/SCGI|"
+			+ "DOCUMENT_ROOT|/usr/share/nginx/html|"
+			+ "SERVER_PROTOCOL|HTTP/1.1|"
+			+ "REMOTE_ADDR|55.555.5.55|"
+			+ "REMOTE_PORT|55555|"
+			+ "SERVER_PORT|80|"
+			+ "SERVER_NAME|localhost|"
+			+ "HTTP_HOST|55.555.555.55|"
+			+ "HTTP_USER_AGENT|Mozilla/5.0 (Windows NT 6.3; WOW64; rv:26.0) Gecko/20100101 Firefox/26.0|"
+			+ "HTTP_ACCEPT|application/xml, text/xml, */*; q=0.01|"
+			+ "HTTP_ACCEPT_LANGUAGE|en-US,en;q=0.5|"
+			+ "HTTP_ACCEPT_ENCODING|gzip, deflate|"
+			+ "HTTP_CONTENT_TYPE|text/xml; charset=UTF-8|"
+			+ "HTTP_X_REQUESTED_WITH|XMLHttpRequest|"
+			+ "HTTP_REFERER|http://55.555.555.55/test.html|"
+			+ "HTTP_CONTENT_LENGTH|81|"
+			+ "HTTP_CONNECTION|keep-alive|"
+			+ "HTTP_PRAGMA|no-cache|"
+			+ "HTTP_CACHE_CONTROL|no-cache|"
+			+ ",<methodCall><methodName>system.library_version</methodName><params/></methodCall>", "|", "\0");
+	public static final ImmutableMap<String, String> REQUEST_HEADERS = ImmutableMap.<String, String>builder()
+			.put("REQUEST_METHOD", "POST")
+			.put("REQUEST_URI", "/SCGI")
+			.put("QUERY_STRING", "")
+			.put("CONTENT_TYPE", "text/xml; charset=UTF-8")
+			.put("DOCUMENT_URI", "/SCGI")
+			.put("DOCUMENT_ROOT", "/usr/share/nginx/html")
+			.put("SERVER_PROTOCOL", "HTTP/1.1")
+			.put("REMOTE_ADDR", "55.555.5.55")
+			.put("REMOTE_PORT", "55555")
+			.put("SERVER_PORT", "80")
+			.put("SERVER_NAME", "localhost")
+			.put("HTTP_HOST", "55.555.555.55")
+			.put("HTTP_USER_AGENT", "Mozilla/5.0 (Windows NT 6.3; WOW64; rv:26.0) Gecko/20100101 Firefox/26.0")
+			.put("HTTP_ACCEPT", "application/xml, text/xml, */*; q=0.01")
+			.put("HTTP_ACCEPT_LANGUAGE", "en-US,en;q=0.5")
+			.put("HTTP_ACCEPT_ENCODING", "gzip, deflate")
+			.put("HTTP_CONTENT_TYPE", "text/xml; charset=UTF-8")
+			.put("HTTP_X_REQUESTED_WITH", "XMLHttpRequest")
+			.put("HTTP_REFERER", "http://55.555.555.55/test.html")
+			.put("HTTP_CONTENT_LENGTH", "81")
+			.put("HTTP_CONNECTION", "keep-alive")
+			.put("HTTP_PRAGMA", "no-cache")
+			.put("HTTP_CACHE_CONTROL", "no-cache")
+			.build();
+	public static final String REQUEST_BODY = "<methodCall><methodName>system.library_version</methodName><params/></methodCall>";
+}
