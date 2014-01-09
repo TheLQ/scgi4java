f34103db170471c6f4c27f62c334ceb7c6223a14
Leon Blakey <lord.quackstar@gmail.com>
2014-01-09 17:14:11 -0500

    Added client response parsing

--- scgi/src/main/java/org/thelq/scgi4java/scgi/SCGIClient.java
+++ scgi/src/main/java/org/thelq/scgi4java/scgi/SCGIClient.java
@@ -5,7 +5,11 @@
  */
 package org.thelq.scgi4java.scgi;
 
+import java.io.IOException;
+import java.io.InputStream;
+import java.nio.charset.Charset;
 import java.util.Collections;
+import java.util.HashMap;
 import java.util.Map;
 import lombok.NonNull;
 
@@ -39,5 +43,44 @@ public class SCGIClient {
 
 	protected static String makeRequestHeader(String key, String value) {
 		return key + "\0" + value + "\0";
+	}
+	
+	protected static Map<String, String> parseResponse(InputStream input) throws IOException {
+		return parseResponse(input, Charset.defaultCharset());
+	}
+	
+	protected static Map<String, String> parseResponse(InputStream input, Charset charset) throws IOException {
+		Map<String, String> responseHeaders = new HashMap<String, String>();
+		
+		StringBuilder valBuilder = new StringBuilder();
+		String lastKey = null;
+		char curChar;
+		while ((curChar = (char) input.read()) != (char)-1) {
+			if(curChar == ':' && lastKey == null) {
+				//Finished building the key name
+				lastKey = valBuilder.toString();
+				valBuilder.delete(0, valBuilder.length());
+			} else if(curChar == ' ' && valBuilder.length() == 0) {
+				//This is the spacing after the key but before the value
+			} else if(curChar == '\r') {
+				//Finished building value
+				if(valBuilder.length() != 0) {
+					//JUST finished
+					responseHeaders.put(lastKey, valBuilder.toString());
+					lastKey = null;
+					valBuilder.delete(0, valBuilder.length());
+				} else if(lastKey == null) {
+					//No key and no value, this is the blank line before the body
+					break;
+				}
+			} else if(curChar == '\n') {
+				//Real end of line, ignore
+			} else {
+				//Currently building something
+				valBuilder.append(curChar);
+			}
+		}
+		
+		return responseHeaders;
 	}
 }
