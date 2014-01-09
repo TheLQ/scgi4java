5c1fedc6daf0dd9ed00e316ea5d40228d5b428c5
Leon Blakey <lord.quackstar@gmail.com>
2014-01-09 17:14:22 -0500

    New test for client response parsing

--- scgi/src/test/java/org/thelq/scgi4java/scgi/SCGIClientTest.java
+++ scgi/src/test/java/org/thelq/scgi4java/scgi/SCGIClientTest.java
@@ -5,6 +5,13 @@
  */
 package org.thelq.scgi4java.scgi;
 
+import com.google.common.collect.ImmutableMap;
+import com.google.common.collect.MapDifference;
+import com.google.common.collect.Maps;
+import java.io.ByteArrayInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.Map;
 import org.testng.Assert;
 import org.testng.annotations.Test;
 
@@ -18,5 +25,21 @@ public class SCGIClientTest {
 		String requestCreated = SCGIClient.makeRequest(TestUtils.REQUEST_BODY, TestUtils.REQUEST_HEADERS);
 
 		Assert.assertEquals(TestUtils.REQUEST_RAW, requestCreated, "Generated request doesn't match given");
+	}
+	
+	@Test
+	public void parseResponseTest() throws IOException {
+		InputStream responseStream = new ByteArrayInputStream(TestUtils.RESPONSE_RAW.getBytes());
+		Map<String, String> parsedResponse = SCGIClient.parseResponse(responseStream);
+		
+		//Check response againast actual values
+		ImmutableMap<String, String> fullHeaders = ImmutableMap.<String, String>builder()
+				.putAll(TestUtils.RESPONSE_HEADERS)
+				.put("Status", "200 OK")
+				.put("Content-Type", "text/plain")
+				.put("Content-Length", "5")
+				.build();
+		MapDifference<String, String> headerDiff = Maps.difference(parsedResponse, fullHeaders);
+		Assert.assertEquals(headerDiff.entriesDiffering().size(), 0, "Headers do not match given" + headerDiff);
 	}
 }
