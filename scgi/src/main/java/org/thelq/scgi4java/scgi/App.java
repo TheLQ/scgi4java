1604d9fa54960bb6e47f6f7cd9ead8bce4f5f920
Leon Blakey <lord.quackstar@gmail.com>
2014-01-09 09:56:21 -0500

    Project init

---
+++ scgi/src/main/java/org/thelq/scgi4java/scgi/App.java
@@ -0,0 +1,13 @@
+package org.thelq.scgi4java.scgi;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
